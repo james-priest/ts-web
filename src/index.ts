1f60e45c710c514a157b5d2d85389349d3869e28
James Priest <james-priest@outlook.com>
2020-01-02 18:45:10 -0800

    feat: Create Attributes class

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,3 @@
 import { User } from './models/User';
 
-// Update existing record
 const user = new User({ id: 1 });
-// user.set({ name: 'New Name', age: 999 });
-// user.save();
-user.events.on('change', () => {
-  console.log('change!');
-});
-user.events.trigger('change');
-
-// Add new record
-// const user = new User({ name: 'new record', age: 21 });
-// user.save();
---
+++ src/models/Attributes.ts
@@ -0,0 +1,12 @@
+export class Attributes<T> {
+  constructor(private data: T) {}
+
+  // generic constraint; K can only ever be a key of T
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key];
+  }
+
+  set(update: T): void {
+    Object.assign(this.data, update);
+  }
+}
