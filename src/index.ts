2be5afdaae3b6df536eb4b1ff8f7c05f2a04410a
James Priest <james-priest@outlook.com>
2020-01-04 16:05:28 -0800

    refactor: Extract User to Model

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 import { User } from './models/User';
 
-// const user = new User({ id: 1 });
-// user.on('change', () => console.log(user));
-// user.fetch();
+const user = User.buildUser({ id: 1 });
+user.on('change', () => console.log(user));
 
-const user = new User({ id: 1, name: 'newer name', age: 0 });
-user.on('save', () => console.log(user));
-
-user.save();
+user.fetch();
