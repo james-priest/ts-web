699faa98195b7ec8aab999efc91714ba16ad7f13
James Priest <james-priest@outlook.com>
2020-01-04 16:57:10 -0800

    feat: Create Collection class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import { User } from './models/User';
+import { Collection } from './models/Collection';
 
-const user = User.buildUser({ id: 1 });
-user.on('change', () => console.log(user));
+const collection = new Collection('http://localhost:3000/users');
 
-user.fetch();
+collection.on('change', () => console.log(collection));
+
+collection.fetch();
--- src/models/Model.ts
+++ src/models/Model.ts
@@ -28,17 +28,12 @@ export class Model<T extends HasId> {
   ) {}
 
   // pass-through methods...
-  get on() {
-    return this.events.on;
-  }
 
-  get trigger() {
-    return this.events.trigger;
-  }
-
-  get get() {
-    return this.attributes.get;
-  }
+  // shortened syntax
+  // doesn't work if dependencies defined outside of constructor
+  on = this.events.on;
+  trigger = this.events.trigger;
+  get = this.attributes.get;
 
   set(update: T): void {
     this.attributes.set(update);
