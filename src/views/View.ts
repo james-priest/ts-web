045c2ffefebcc6c5fbb6f2d7057ddc2e82c547c3
James Priest <james-priest@outlook.com>
2020-01-09 18:03:23 -0800

    feat: Nesting of views

--- src/views/View.ts
+++ src/views/View.ts
@@ -6,15 +6,24 @@ interface ModelForView {
 }
 
 // Whenever we create view, we will pass in two generic types.
-// T which applies generic constraint of Model.
-// and K which will be used as the type for Model.
+//   T which applies generic constraint of Model.
+//   and K which will be used as the type for Model.
 // T will have all same props as Model<K> with K loaded into Model.
 export abstract class View<T extends Model<K>, K> {
-  abstract eventsMap(): { [key: string]: () => void };
-  abstract template(): string;
+  regions: { [key: string]: Element } = {};
 
   constructor(public parent: Element, public model: T) {
     this.bindModel();
+  }
+
+  abstract template(): string;
+
+  regionsMap(): { [key: string]: string } {
+    return {};
+  }
+
+  eventsMap(): { [key: string]: () => void } {
+    return {};
   }
 
   bindModel(): void {
@@ -35,16 +44,31 @@ export abstract class View<T extends Model<K>, K> {
     }
   }
 
+  mapRegions(fragment: DocumentFragment): void {
+    const regionsMap = this.regionsMap();
+
+    for (const key in regionsMap) {
+      const selector = regionsMap[key];
+      const element = fragment.querySelector(selector);
+
+      if (element) {
+        this.regions[key] = element;
+      }
+    }
+  }
+
+  onRender(): void {}
+
   render(): void {
     this.parent.innerHTML = '';
 
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
 
-    // templateElement.content
-    //   .querySelector('button')
-    //   .addEventListener('click', this.onButtonClick);
     this.bindEvents(templateElement.content);
+    this.mapRegions(templateElement.content);
+
+    this.onRender();
 
     this.parent.append(templateElement.content);
   }
